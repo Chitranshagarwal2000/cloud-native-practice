9768e1e79e217f72ee68a56a777ab65aed17e0f6
Chitransh Agarwal <chitranshagarwal2000@gmail.com>
2023-08-27 03:48:43 +0530

    Added Flyway migration

--- catalog-service/src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
+++ catalog-service/src/main/java/com/polarbookshop/catalogservice/config/DataConfig.java
@@ -1,6 +1,5 @@
 package com.polarbookshop.catalogservice.config;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
 
