fcda8d3d4d26a3020489fcd827b4fd90c2c59c54
Chitransh Agarwal <chitranshagarwal2000@gmail.com>
2023-08-26 21:04:58 +0530

    Added Flyway migration

--- catalog-service/src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
+++ catalog-service/src/test/java/com/polarbookshop/catalogservice/web/BookJsonTests.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.json.JsonContent;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
+@ActiveProfiles("integration")
 class BookJsonTests {
 
     @Autowired
